49c077d791e2f51f5a2156ba4ccf7d640de1f55a
mac <laurisalmawa05@gmail.com>
2024-11-21 23:16:49 +0100

    function added

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,6 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Bienvenido a JavaBank");
+        System.out.println("Welcome to JavaBank ATM, git version");
     }
 }
